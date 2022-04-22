e84dc3da2618bcf476e21cd11b9187d45139d9bd
sneha-james-j <77431005+sneha-james-j@users.noreply.github.com>
2022-04-22 16:21:58 +0530

    Add files via upload

---
+++ task2.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+void main()
+{
+    int num;
+    printf("Enter a number: \n");
+    scanf("%d",&num);
+    if (num>0) 
+         printf("%d is a positive number \n",num);
+    
+        else
+            printf("%d is a negative number \n",num);
+    
+}
